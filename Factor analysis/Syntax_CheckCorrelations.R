6ecb0aeda1756c4ae144e478fe8043e43da82824
Sebastian Jentschke <37706914+sjentsch@users.noreply.github.com>
2022-04-28 12:38:18 +0200

    Update Syntax_CheckCorrelations.R

--- Factor analysis/Syntax_CheckCorrelations.R
+++ Factor analysis/Syntax_CheckCorrelations.R
@@ -1,4 +1,14 @@
 # calculate a correlation for all possible combinations of variables
+# ===========================================================================
+# It is important that you adjust [2:26] in the command underneath to your
+# number of columns; in the bfi_sample dataset (that the code was written
+# for), we have first the "ID" (which we don't want to include in the cal-
+# culation), and afterwards the variables "A1" to "O5" (that we need).
+# Given that these are 25 variables and that we exclude the first column,
+# we have to calculate correlations for the 2nd to the 26th column, i.e.,
+# [2:26]
+# For SAQ, are the columns [1:23], for Williams [4:31] and for TOSSE-R [1:28]
+# ===========================================================================
 crrMtx = abs(cor(sapply(data[2:26], jmvcore::toNumeric), use = "pairwise"))
 # the correlation with itself is always 1, we don't want to include that
 diag(crrMtx) <- NA
