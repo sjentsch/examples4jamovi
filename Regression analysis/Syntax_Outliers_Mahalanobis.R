7e641aff6cc2420598beae80db1dd6ad8a3ed7cd
Sebastian Jentschke <Sebastian.Jentschke@gmail.com>
2023-11-23 15:55:53 +0100

    Changed Mahalanobis-syntax to output a filter expression

--- Regression analysis/Syntax_Outliers_Mahalanobis.R
+++ Regression analysis/Syntax_Outliers_Mahalanobis.R
@@ -1,10 +1,19 @@
 # this list should contain the names of your INDEPENDENT VARIABLES
 # you should not include your dependent variables
 VL = c('dan.sleep', 'baby.sleep', 'day')
-# the second line calculates the Mahalanobis distance, and the third
-# line the critical chi-square value; if the Mahalanobis distance is
-# above the critical value, which will find the entry (which finds
-# logical TRUE values) and names will output the row number
-names(which(
-  mahalanobis(data[, VL], colMeans(data[, VL]), cov(data[, VL])) >
-  qchisq(p = 0.001, df = length(VL), lower.tail = FALSE)))
+# the lines underneath calculate the Mahalanobis distance (2), and
+# whether it is above the critical chi-square value (3); if the
+# Mahalanobis distance is above the critical value which(names(...
+# will output the row number
+outRow <- names(which(
+    mahalanobis(data[, VL], colMeans(data[, VL]), cov(data[, VL])) >
+    qchisq(p = 0.001, df = length(VL), lower.tail = FALSE)))
+# the lines underneath check whether rows with outliers were found
+# if so, a string that can be used as filter expression is output,
+# otherwise a notice that no outliers were found
+if (length(outRow) > 0) {
+    # add this output to a filter to exclude
+    cat(paste0(paste0("ROW() != ", c("", selRow)), collapse = " and "))
+} else {
+    cat("There were no outliers found.")
+}
